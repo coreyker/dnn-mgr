981c90e157dfa8ce9f4381f6bf171130745671f5
Corey K <cmke@comp-nbits2143.imm.dtu.dk>
2014-11-03 12:41:12 +0100

    fix gpu pickled files to run on cpu

---
+++ gpu_to_cpu_pkl.py
@@ -0,0 +1,15 @@
+import os, sys
+from pylearn2.utils import serial
+import pylearn2.config.yaml_parse as yaml_parse
+
+if __name__=="__main__":
+	
+	_, in_path, out_path = sys.argv
+	os.environ['THEANO_FLAGS']="device=cpu"
+  
+	model = serial.load(in_path)
+ 
+	model2 = yaml_parse.load(model.yaml_src)
+	model2.set_param_values(model.get_param_values())
+ 
+	serial.save(out_path, model2)
