f52d96d83dbdf4f0858083c201e55d7adaa2d04c
Corey K <cmke@comp-nbits2143.imm.dtu.dk>
2014-11-03 13:01:51 +0100

    na

--- gpu_to_cpu_pkl.py
+++ gpu_to_cpu_pkl.py
@@ -1,15 +1,24 @@
-import os, sys
+import os, sys, glob
 from pylearn2.utils import serial
 import pylearn2.config.yaml_parse as yaml_parse
 
 if __name__=="__main__":
-	
-	_, in_path, out_path = sys.argv
+
 	os.environ['THEANO_FLAGS']="device=cpu"
-  
-	model = serial.load(in_path)
- 
-	model2 = yaml_parse.load(model.yaml_src)
-	model2.set_param_values(model.get_param_values())
- 
-	serial.save(out_path, model2)
+	_, directory = sys.argv
+
+	for in_file in glob.glob(os.path.join(directory + '*.pkl'):
+		if in_file.split('.')[-2] == 'cpu':
+			continue
+		
+		out_file = os.path.splitext(d)[0] + '.cpu.pkl'
+		
+		if os.path.exists(out_file):
+			continue
+
+		model = serial.load(in_path)
+	 
+		model2 = yaml_parse.load(model.yaml_src)
+		model2.set_param_values(model.get_param_values())
+	 
+		serial.save(out_path, model2)
