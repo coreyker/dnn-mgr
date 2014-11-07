e037b64b30f733e4052efcdcd739ab347536f592
Corey K <cmke@comp-nbits2143.imm.dtu.dk>
2014-11-07 14:23:01 +0100

    adjust yamlsrc pointer, not tested

--- gpu_to_cpu_pkl.py
+++ gpu_to_cpu_pkl.py
@@ -1,4 +1,4 @@
-import os, sys, glob
+import os, sys, glob, re
 import numpy as np
 from pylearn2.utils import serial
 import pylearn2.config.yaml_parse as yaml_parse
@@ -12,8 +12,12 @@ if __name__=="__main__":
 
 	files_list = glob.glob(os.path.join(directory, '*.pkl'))
 
+	p1 = re.compile(r"(cpu)")
+	p2 = re.compile(r"(\.pkl)")
+
 	for in_file in files_list:
-		if in_file.split('.')[-2] == 'cpu':
+
+		if p.search(in_file) != None:
 			continue
 
 		out_file = os.path.splitext(in_file)[0] + '.cpu.pkl'
@@ -27,7 +31,7 @@ if __name__=="__main__":
 		params = model.get_param_values()
 		model2.set_param_values(params)
 
-		model2.yaml_src = model.yaml_src
+		model2.yaml_src = p2.sub('.cpu.pkl', model.yaml_src)
 		model2.dataset_yaml_src = model.dataset_yaml_src
 
 		serial.save(out_file, model2)
