fixed confusion normalisation in test_mlp_script, added pretty printing, added ability to pass in dataset from command line

GTZAN_dataset.py
@@ -39,6 +39,7 @@ class GTZAN_iterator(FiniteDatasetIterator):
             When there are no more batches to return.
         """
         next_index = self._subset_iterator.next()
+        #pdb.set_trace()
         next_index = self._dataset.support[ next_index ] # !!! added line to iterate over different index set !!!
 
         # !!! fancy indexing doesn't seem to work w/ pytables (data[next_index] doesn't work, but data[next_index,:] does) !!!
@@ -171,7 +172,7 @@ class GTZAN_standardizer(Block):
 
         self._mean = np.array(config['mean'], dtype=np.float32)
         self._std  = np.array(config['std'], dtype=np.float32)
-        self.input_space = VectorSpace(len(self._mean))
+        self.input_space = VectorSpace(np.prod(self._mean.shape))
 
         super(GTZAN_standardizer, self).__init__()
 

GTZAN_dataset2d.py
@@ -52,7 +52,7 @@ class GTZAN_iterator2d(FiniteDatasetIterator):
         # alternative for 2d data topology
         n_frames_per_sample = self._dataset.n_frames_per_sample
         spaces, sources = self._data_specs
-        output = []
+        output = []                
 
         for data, fn, source in safe_izip(self._raw_data, self._convert, sources):
             if source=='targets':
@@ -96,7 +96,8 @@ class GTZAN_dataset2d(DenseDesignMatrixPyTables):
         self.n_frames_per_file = config['n_frames_per_file']
         self.n_frames_per_sample = config['n_frames_per_sample']
         
-        view_converter = DefaultViewConverter((513, self.n_frames_per_sample, 1))
+        #!!!nb: 513 shouldn't be hardcoded here!!!
+        view_converter = DefaultViewConverter((self.n_frames_per_sample, 513, 1))
         
         super(GTZAN_dataset2d, self).__init__(X=data.X, y=data.y,
             view_converter=view_converter)
@@ -204,11 +205,17 @@ class GTZAN_standardizer2d(Block):
 
     def __init__(self, config):
 
-        self._mean = np.array(config['mean'], dtype=np.float32)
-        self._std  = np.array(config['std'], dtype=np.float32)
-        self.input_space = VectorSpace(len(self._mean))
+        # ...this doesn't work:
+        #self._mean = np.array(config['mean'], dtype=np.float32)
+        #self._std  = np.array(config['std'], dtype=np.float32)
+        #self.input_space = Conv2DSpace(shape=self._mean.shape, num_channels=1, axes=('b', 'c', 0, 1))
 
-        super(GTZAN_standardizer, self).__init__()
+        shape = (np.prod(config['mean'].shape), )
+        self._mean = np.reshape(np.array(config['mean'], dtype=np.float32), shape)
+        self._std  = np.reshape(np.array(config['std'], dtype=np.float32), shape)
+        self.input_space = VectorSpace(dim=shape[0])
+        
+        super(GTZAN_standardizer2d, self).__init__()
 
     def __call__(self, batch):
         """

prepare_dataset.py
@@ -56,7 +56,7 @@ def make_h5_dataset(dataset_dir, h5_file_name='GTZAN_1024.h5', n_fft=1024, n_hop
 	h5file.close()
 	print '' # newline
 
-def generate_fold_configs(h5_file_name='GTZAN_1024.h5', n_folds=4, valid_prop=0.333):
+def generate_fold_configs(h5_file_name='GTZAN_1024.h5', n_folds=4, valid_prop=0.333, n_frames_per_sample=1):
 	"""	
 	Generate dataset config files with stratified folds
 	"""	
@@ -88,7 +88,10 @@ def generate_fold_configs(h5_file_name='GTZAN_1024.h5', n_folds=4, valid_prop=0.
 	for fold in xrange(n_folds):
 
 		print 'Creating fold %d of %d' % (fold+1, n_folds)
-		config_file =  os.path.splitext(h5_file_name)[0] + '-fold-%d_of_%d.pkl' % (fold+1, n_folds)
+		if n_frames_per_sample==1:
+			config_file = os.path.splitext(h5_file_name)[0] + '-fold-%d_of_%d.pkl' % (fold+1, n_folds)
+		else:
+			config_file = os.path.splitext(h5_file_name)[0] + '-%d-fold-%d_of_%d.pkl' % (n_frames_per_sample, fold+1, n_folds)
 		
 		if os.path.exists( config_file ):	
 			print config_file + ' already exists, aborting...'
@@ -110,9 +113,9 @@ def generate_fold_configs(h5_file_name='GTZAN_1024.h5', n_folds=4, valid_prop=0.
 			train_files[n] = sorted(rot_perm[n_test_files : n_test_files + n_train_files])
 			valid_files[n] = sorted(rot_perm[n_test_files + n_train_files:])
 
-			test_support[n]  = np.hstack([i * n_frames_per_file + np.arange(n_frames_per_file) for i in test_files[n]])
-			train_support[n] = np.hstack([i * n_frames_per_file + np.arange(n_frames_per_file) for i in train_files[n]])
-			valid_support[n] = np.hstack([i * n_frames_per_file + np.arange(n_frames_per_file) for i in valid_files[n]])
+			test_support[n]  = np.hstack([i * n_frames_per_file + np.arange(0,n_frames_per_file,n_frames_per_sample) for i in test_files[n]])
+			train_support[n] = np.hstack([i * n_frames_per_file + np.arange(0,n_frames_per_file,n_frames_per_sample) for i in train_files[n]])
+			valid_support[n] = np.hstack([i * n_frames_per_file + np.arange(0,n_frames_per_file,n_frames_per_sample) for i in valid_files[n]])
 		
 		test_files    = sum(test_files,[])
 		train_files   = sum(train_files,[])
@@ -122,15 +125,15 @@ def generate_fold_configs(h5_file_name='GTZAN_1024.h5', n_folds=4, valid_prop=0.
 		valid_support = np.hstack(valid_support)
 
 		# compute mean and std for training set only
-		sum_x  = np.zeros(n_feats, dtype=np.float32)
-		sum_x2 = np.zeros(n_feats, dtype=np.float32)	
+		sum_x  = np.zeros((n_frames_per_sample,n_feats), dtype=np.float32)
+		sum_x2 = np.zeros((n_frames_per_sample,n_feats), dtype=np.float32)	
 		n_samples = len(train_support)
 		
 		for n,i in enumerate(train_support):
 			sys.stdout.write('\rProgress: %2.2f%%' % (n/float(n_samples)*100))
 			sys.stdout.flush()
 			
-			fft_frame = data.X[i,:]
+			fft_frame = data.X[i:i+n_frames_per_sample,:]
 			sum_x  += fft_frame
 			sum_x2 += fft_frame**2
 		print ''
@@ -138,9 +141,12 @@ def generate_fold_configs(h5_file_name='GTZAN_1024.h5', n_folds=4, valid_prop=0.
 		mean = sum_x / n_samples
 		var  = (sum_x2 - sum_x**2/n_samples)/(n_samples-1)
 
+		# add PCA whitening and dimensionality reductions for spectrogram patches....?
+
 		config = {
 			'h5_file_name': h5_file_name,
 			'n_frames_per_file': n_frames_per_file,
+			'n_frames_per_sample' : n_frames_per_sample,
 			'test': test_support, 
 			'train': train_support, 
 			'valid': valid_support,
@@ -157,14 +163,18 @@ def generate_fold_configs(h5_file_name='GTZAN_1024.h5', n_folds=4, valid_prop=0.
 
 	h5file.close()
 
-def generate_folds_from_files(h5_file_name=None, train_file=None, valid_file=None, test_file=None):
+def generate_folds_from_files(h5_file_name=None, train_file=None, valid_file=None, test_file=None, n_frames_per_sample=1):
 
 	assert h5_file_name is not None
 	assert train_file is not None
 	assert valid_file is not None
 	assert test_file is not None
 
-	config_file = os.path.splitext(h5_file_name)[0] + '-filtered-fold.pkl'
+	if n_frames_per_sample==1:
+		config_file = os.path.splitext(h5_file_name)[0] + '-filtered-fold.pkl'
+	else:
+		config_file = os.path.splitext(h5_file_name)[0] + '-%d-filtered-fold.pkl' % n_frames_per_sample
+
 	if os.path.exists( config_file ):	
 		print config_file + ' already exists, aborting...'
 		return
@@ -189,20 +199,20 @@ def generate_folds_from_files(h5_file_name=None, train_file=None, valid_file=Non
 		lines = f.readlines()
 		test_files = [int(i)-1 for i in lines]				
 
-	test_support  = np.hstack([i * n_frames_per_file + np.arange(n_frames_per_file) for i in test_files])
-	train_support = np.hstack([i * n_frames_per_file + np.arange(n_frames_per_file) for i in train_files])
-	valid_support = np.hstack([i * n_frames_per_file + np.arange(n_frames_per_file) for i in valid_files])
+	test_support  = np.hstack([i * n_frames_per_file + np.arange(0,n_frames_per_file,n_frames_per_sample) for i in test_files])
+	train_support = np.hstack([i * n_frames_per_file + np.arange(0,n_frames_per_file,n_frames_per_sample) for i in train_files])
+	valid_support = np.hstack([i * n_frames_per_file + np.arange(0,n_frames_per_file,n_frames_per_sample) for i in valid_files])
 
 	# compute mean and std for training set only
-	sum_x  = np.zeros(n_feats, dtype=np.float32)
-	sum_x2 = np.zeros(n_feats, dtype=np.float32)
+	sum_x  = np.zeros((n_frames_per_sample,n_feats), dtype=np.float32)
+	sum_x2 = np.zeros((n_frames_per_sample,n_feats), dtype=np.float32)
 	n_samples = len(train_support)
 	
 	for n,i in enumerate(train_support):
 		sys.stdout.write('\rProgress: %2.2f%%' % (n/float(n_samples)*100))
 		sys.stdout.flush()
 		
-		fft_frame = data.X[i,:]
+		fft_frame = data.X[i:i+n_frames_per_sample,:]
 		sum_x  += fft_frame
 		sum_x2 += fft_frame**2
 	print ''
@@ -213,6 +223,7 @@ def generate_folds_from_files(h5_file_name=None, train_file=None, valid_file=Non
 	config = {
 		'h5_file_name': h5_file_name,
 		'n_frames_per_file': n_frames_per_file,
+		'n_frames_per_sample' : n_frames_per_sample,
 		'test': test_support, 
 		'train': train_support, 
 		'valid': valid_support,
@@ -241,4 +252,7 @@ if __name__ == "__main__":
 	generate_fold_configs(h5_file_name)
 
 	generate_folds_from_files(h5_file_name, 'train_filtered.txt', 'valid_filtered.txt', 'test_filtered.txt')
+	
+	#generate_folds_from_files(h5_file_name, 'train_filtered.txt', 'valid_filtered.txt', 'valid_filtered.txt') #'test_filtered_1perartist.txt')
 
+	generate_fold_configs(h5_file_name, n_frames_per_sample=40)

test_mlp_script.py
@@ -70,7 +70,7 @@ def file_misclass_error(model, dataset):
         batch_size=batch_size, 
         data_specs=data_specs
         )
-
+    
     i=0
     for el in iterator:
 
@@ -121,7 +121,7 @@ def file_misclass_error_topx(model, dataset, topx=3):
 
     hits = 0
     n = 0
-    i=0
+    i=0        
     for el in iterator:
 
         # display progress indicator
@@ -148,31 +148,43 @@ def file_misclass_error_topx(model, dataset, topx=3):
     return hits/float(n)*100
 
 
+def pp_array(array): # pretty printing
+    for row in array:
+        print ['%04.1f' % el for el in row]
+
+
 if __name__ == '__main__':
     
-    _, model_file = sys.argv
+    model_file = sys.argv[1]
     
     # get model
     model = serial.load(model_file)  
 
-    # get dataset fold used for training from model's yaml_src
-    p = re.compile(r"which_set.*'(train)'")
-    dataset_yaml = p.sub("which_set: 'test'", model.dataset_yaml_src)
-    dataset = yaml_parse.load(dataset_yaml)
+    if len(sys.argv)>2: # dataset config passed in from command line
+        print 'Using dataset passed in from command line:'
+        with open(sys.argv[2]) as f: cfg = cPickle.load(f)
+        dataset = TransformerDataset(
+            raw=GTZAN_dataset.GTZAN_dataset(which_set='test', config=cfg),
+            transformer=GTZAN_dataset.GTZAN_standardizer(config=cfg)
+            )
+
+    else: # get dataset from model's yaml_src
+        print "Using dataset from model's yaml src:"
+        p = re.compile(r"which_set.*'(train)'")
+        dataset_yaml = p.sub("which_set: 'test'", model.dataset_yaml_src)
+        dataset = yaml_parse.load(dataset_yaml)
 
     # test error
     #err, conf = frame_misclass_error(model, dataset)
     
     err, conf = file_misclass_error(model, dataset)
     
+    conf = conf.transpose()
     print 'test accuracy: %2.2f' % (100-err)
-    print 'confusion matrix:'
-    print conf/np.sum(conf, axis=1)
+    print 'confusion matrix (cols true):'
+    pp_array(100*conf/np.sum(conf, axis=0))
 
     # acc = file_misclass_error_topx(model, dataset, 2)
     # print 'test accuracy: %2.2f' % acc
-
-
-
 
 