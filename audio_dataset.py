d3540dded37217a004cfd41fdbd64da9bf2f7105
Corey K <cmke@bohr.lan>
2014-11-26 18:51:10 +0100

    starting to re-architect so that any generic dataset can be used, audio_dataset.py and prepare_datase2.py should be useful for this

---
+++ audio_dataset.py
@@ -0,0 +1,261 @@
+import numpy as np
+import functools
+import tables
+
+from pylearn2.datasets.dataset import Dataset
+from pylearn2.datasets.transformer_dataset import TransformerDataset
+from pylearn2.datasets.dense_design_matrix import DenseDesignMatrixPyTables
+from pylearn2.blocks import Block
+from pylearn2.space import CompositeSpace, Conv2DSpace, VectorSpace, IndexSpace
+from pylearn2.utils.iteration import SubsetIterator, FiniteDatasetIterator, resolve_iterator_class
+from pylearn2.utils import safe_zip, safe_izip
+
+from pylearn2.datasets import control
+from pylearn2.utils.exc import reraise_as
+from pylearn2.utils.rng import make_np_rng
+from pylearn2.utils import contains_nan
+from theano import config
+
+import pdb
+
+class AudioDataset(DenseDesignMatrixPyTables):
+    def __init__(self, config, which_set='train', standardize=True, pca_whitening=False, ncomponents=None, epsilon=3):
+
+        keys = ['train', 'test', 'valid']
+        assert which_set in keys
+
+        # load hdf5 metadata
+        self.hdf5       = tables.open_file( config['hdf5'], mode='r')
+        data            = self.hdf5.get_node('/', 'Data')
+        param           = self.hdf5.get_node('/', 'Param')
+        self.file_index = param.file_index[0]
+        self.file_dict  = param.file_dict[0]
+        self.label_list = param.label_list[0]
+        self.targets    = param.targets[0]
+        self.nfft       = param.fft[0]['nfft']
+
+        # load parition information
+        self.support   = config[which_set]
+        self.file_list = config[which_set+'_files']
+        self.mean      = config['mean']
+        self.var       = config['var']
+        self.tframes   = config['tframes']
+
+        if (standardize is True) and (pca_whitening is True):
+            raise ValueError("'standardize' and 'pca_whiten' cannot both be True")
+        
+        if ncomponents is None:
+            self.ncomponents = len(self.mean)
+        else:
+            assert ncomponents <= len(self.mean)
+            self.ncomponents = ncomponents
+
+        if (pca_whitening is True):
+            S = config['S'][:self.ncomponents]   # eigenvalues
+            U = config['U'][:,:self.ncomponents] # eigenvectors            
+            self.pca = np.diag(1./(np.sqrt(S) + epsilon)).dot(U.T) 
+
+
+        if standardize is True:
+            self.transform = lambda X: (X-self.mean)/self.var
+        elif pca_whitening is True:
+            self.transform = lambda X: (X-self.mean).dot(self.pca.transpose())
+        else:
+            self.transform = lambda X: X
+
+        super(AudioDataset, self).__init__(X=data.X, y=data.y)
+    
+    def __del__(self):
+        self.hdf5.close()   
+    
+    @functools.wraps(Dataset.iterator)
+    def iterator(self, mode=None, batch_size=None, num_batches=None,
+                 topo=None, targets=None, rng=None, data_specs=None,
+                 return_tuple=False):
+        '''
+        Copied from pylearn2 superclass in order to return custom iterator.
+        Two different iterators are available, depending on the data_specs.
+        1. If the data_specs source is 'features' a framelevel iterator is returned 
+        (each call to next() returns a single frame)
+        2. If the data_specs source is 'songlevel-features' a songlevel iterator is returned
+        (each call to next() returns all the frames associated with a given song in the dataset)
+        '''
+        if data_specs is None:
+            data_specs = self._iter_data_specs
+        else:
+            self.data_specs = data_specs
+
+        # If there is a view_converter, we have to use it to convert
+        # the stored data for "features" into one that the iterator
+        # can return.
+        space, source = data_specs
+        if isinstance(space, CompositeSpace):
+            sub_spaces = space.components
+            sub_sources = source
+        else:
+            sub_spaces = (space,)
+            sub_sources = (source,)
+
+        convert = []
+        for sp, src in safe_zip(sub_spaces, sub_sources):
+            if src == 'features' and \
+               getattr(self, 'view_converter', None) is not None:
+                conv_fn = (lambda batch, self=self, space=sp:
+                           self.view_converter.get_formatted_batch(batch,
+                                                                   space))
+            else:
+                conv_fn = None
+
+            convert.append(conv_fn)
+
+        # TODO: Refactor
+        if mode is None:
+            if hasattr(self, '_iter_subset_class'):
+                mode = self._iter_subset_class
+            else:
+                raise ValueError('iteration mode not provided and no default '
+                                 'mode set for %s' % str(self))
+        else:
+            mode = resolve_iterator_class(mode)
+
+        if batch_size is None:
+            batch_size = getattr(self, '_iter_batch_size', None)
+        if num_batches is None:
+            num_batches = getattr(self, '_iter_num_batches', None)
+        if rng is None and mode.stochastic:
+            rng = self.rng
+
+        if 'songlevel-features' in sub_sources:
+            return SonglevelIterator(self,
+                              mode(len(self.file_list), batch_size, num_batches, rng),
+                              data_specs=data_specs,
+                              return_tuple=return_tuple,
+                              convert=convert)
+        else:
+            return FramelevelIterator(self,
+                                  mode(len(self.support), batch_size, num_batches, rng),
+                                  data_specs=data_specs,
+                                  return_tuple=return_tuple,
+                                  convert=convert)
+
+class FramelevelIterator(FiniteDatasetIterator):
+    '''
+    Returns individual (spectrogram) frames/slices from the dataset
+    '''
+    @functools.wraps(SubsetIterator.next)
+    def next(self):
+        """
+        Retrieves the next batch of examples.
+
+        Returns
+        -------
+        next_batch : object
+            An object representing a mini-batch of data, conforming
+            to the space specified in the `data_specs` constructor
+            argument to this iterator. Will be a tuple if more
+            than one data source was specified or if the constructor
+            parameter `return_tuple` was `True`.
+
+        Raises
+        ------
+        StopIteration
+            When there are no more batches to return.
+        """
+        next_index = self._subset_iterator.next()
+        next_index = self._dataset.support[ next_index ] # !!! added line to iterate over different index set !!!
+
+        spaces, sources = self._data_specs
+        output = []                
+
+        for data, fn, source in safe_izip(self._raw_data, self._convert, sources):
+            if source=='targets':
+                if fn:
+                    output.append( fn(data[next_index, :]) )
+                else:
+                    output.append( data[next_index, :] )
+            else:
+                design_mat = []
+                for index in next_index:
+                    X = self._dataset.transform( data[index:index+self._dataset.tframes, :] )                                    
+                    design_mat.append( X.reshape((np.prod(X.shape),)) )                    
+                design_mat = np.vstack(design_mat)
+
+                if fn:
+                    output.append( fn(design_mat) )
+                else:
+                    output.append( design_mat )
+
+        rval = tuple(output)
+        if not self._return_tuple and len(rval) == 1:
+            rval, = rval
+        return rval
+
+class SonglevelIterator(FiniteDatasetIterator):
+    '''
+    Returns all data associated with a particular song from the dataset
+    '''
+    @functools.wraps(SubsetIterator.next)
+    def next(self):
+        
+        # next numerical index
+        next_file_index = self._subset_iterator.next()        
+        
+        # associate numerical index with file from the dataset
+        next_file = self._dataset.file_list[ next_file_index ] # !!! added line to iterate over different index set !!!
+        
+        # lookup file's position in the hdf5 array
+        next_index = []
+        for f in next_file:
+            offset, nframes, key, target = self._dataset.file_index[f]
+            next_index.append(offset + np.arange(nframes))
+        next_index = np.hstack(next_index)
+
+
+        spaces, sources = self._data_specs
+        output = []                
+
+        for data, fn, source in safe_izip(self._raw_data, self._convert, sources):
+            if source=='targets':
+                if fn:
+                    output.append( fn(data[next_index, :]) )
+                else:
+                    output.append( data[next_index, :] )
+            else:
+                design_mat = []
+                for index in next_index:
+                    X = self._dataset.transform( data[index:index+self._dataset.tframes, :] )                                    
+                    design_mat.append( X.reshape((np.prod(X.shape),)) )                    
+                design_mat = np.vstack(design_mat)
+
+                if fn:
+                    output.append( fn(design_mat) )
+                else:
+                    output.append( design_mat )
+                    
+        rval = tuple(output)
+        if not self._return_tuple and len(rval) == 1:
+            rval, = rval
+        return rval
+
+if __name__=='__main__':
+
+    # tests
+    import cPickle
+
+    with open('/Users/cmke/Datasets/tzanetakis_genre/tzanetakis_genre-fold-1_of_4.pkl') as f: 
+        config = cPickle.load(f)
+    
+    D = AudioDataset(config, standardize=False, pca_whitening=True, ncomponents=50)
+    
+    feat_space   = VectorSpace(dim=D.ncomponents)    
+    target_space = VectorSpace(dim=len(D.label_list))
+    
+    data_specs_frame = (CompositeSpace((feat_space,target_space)), ("features", "targets"))
+    data_specs_song = (CompositeSpace((feat_space,target_space)), ("songlevel-features", "targets"))
+
+    framelevel_it = D.iterator(mode='sequential', batch_size=10, data_specs=data_specs_frame)
+    frame_batch = framelevel_it.next()
+
+    songlevel_it = D.iterator(mode='sequential', batch_size=1, data_specs=data_specs_song)    
+    song_batch = songlevel_it.next()
+
---
+++ prepare_dataset2.py
@@ -0,0 +1,360 @@
+import os, sys, re
+import warnings
+import numpy as np
+import tables
+import theano
+import cPickle
+from scikits import audiolab
+
+import pdb
+
+def collect_audio(root_directory, label_list):
+	'''
+	Find all audio files in the given directory and subdirectories. 
+	Associate each file with a label:
+	1. If the file resides in a directory whos name is in the label_list, apply that label
+	2. If the file's name contains a label from the label list, apply that label
+	
+	1 takes precidence over 2.
+
+	root_directory: base directory to start search
+	label_list: list of categorical (alphanumeric) labels
+	'''
+	pwd = os.getcwd()
+	os.chdir(root_directory) # is this bad? (chdir to get relative path names later on)
+	label_list = [label.lower() for label in label_list] # make sure labels are lowercase
+	
+	file_dict = dict.fromkeys(label_list)
+	for key in file_dict: file_dict[key]=list()
+
+	for root, dirs, files in os.walk(root_directory):
+	    for filename in files:
+	        if filename.endswith(('.wav', '.au')):
+	            # audio file found, check for label
+	            labelled = False
+	            dir_label = os.path.split(root)[-1].lower()
+	            if dir_label in label_list:
+	            	file_dict[dir_label].append(os.path.relpath(os.path.join(root, filename), '.'))
+	            	labelled = True
+	            else:
+	            	for label in label_list:
+	            		match = re.search(label, filename, re.IGNORECASE)
+	            		if match:
+	            			file_dict[match.group().lower()].append(os.path.relpath(os.path.join(root, filename), '.'))
+	            			labelled = True
+	            			break
+	            if labelled is not True:
+	            	warnings.warn('Found audio file %s, but could not determine its label' % filename)
+	os.chdir(pwd)
+
+	for key in file_dict:
+		print 'Found %d audio files with label %s' % (len(file_dict[key]), key)
+
+	return file_dict
+
+def make_hdf5(hdf5_save_name, label_list, root_directory='.', nfft=1024, nhop=512):
+
+	if os.path.exists(hdf5_save_name):
+		warnings.warn('hdf5 file {} already exists, new file will not be created'.format(hdf5_save_name))
+		return
+
+	file_dict  = collect_audio(root_directory, label_list)	
+
+	# hdf5 setup
+	hdf5_file  = tables.open_file(hdf5_save_name, mode = "w")
+	data_node  = hdf5_file.create_group(hdf5_file.root, "Data", "Data")
+	data_atom  = tables.Float32Atom() if theano.config.floatX == 'float32' else tables.Float64Atom()
+
+	# data nodes
+	hdf5_file.create_earray(data_node, 'X', atom=data_atom, shape=(0,nfft/2+1), title="features")
+	hdf5_file.create_earray(data_node, 'y', atom=data_atom, shape=(0,len(label_list)), title="targets")
+
+	targets = range(len(label_list))
+	window = np.hanning(nfft)
+
+	file_index = {}
+	offset = 0
+	for target, key in zip(targets, file_dict):
+		print 'Processing %s' % key
+
+		for f in file_dict[key]:
+			
+			if f.endswith('.wav'):
+				read_fun = audiolab.wavread				
+			elif f.endswith('.au'):
+				read_fun = audiolab.auread
+			
+			audio_data, fs, _ = read_fun(os.path.join(root_directory, f))
+			
+			# compute dft
+			nframes  = (len(audio_data)-nfft)//nhop
+			fft_data = np.zeros((nframes, nfft))
+
+			for i in xrange(nframes):
+				sup = i*nhop + np.arange(nfft)
+				fft_data[i,:] = audio_data[sup] * window
+			
+			fft_data = np.abs(np.fft.fft(fft_data))
+
+			# write dft frames to hdf5 file
+			data_node.X.append(fft_data[:, :nfft/2+1])
+
+			# write target values to hdf5 file
+			one_hot = np.zeros((nframes, len(label_list)))
+			one_hot[:,target] = 1
+			data_node.y.append(one_hot)
+
+			# keep file-level info
+			file_index[f] = (offset, nframes, key, target)
+			offset += nframes
+
+			hdf5_file.flush()
+
+	# write file_index and dft parameters to hdf5 file
+	param_node = hdf5_file.create_group(hdf5_file.root, "Param", "Param")
+	param_atom = tables.ObjectAtom()
+	
+	# save dataset metadata
+	hdf5_file.create_vlarray(param_node, 'file_index', atom=param_atom, title='file_index')
+	param_node.file_index.append(file_index)
+	
+	hdf5_file.create_vlarray(param_node, 'file_dict', atom=param_atom, title='file_dict')
+	param_node.file_dict.append(file_dict)
+
+	hdf5_file.create_vlarray(param_node, 'fft', atom=param_atom, title='fft')
+	param_node.fft.append({'nfft':nfft, 'nhop':nhop})
+
+	hdf5_file.create_vlarray(param_node, 'label_list', atom=param_atom, title='label_list')
+	param_node.label_list.append(label_list)
+	
+	hdf5_file.create_vlarray(param_node, 'targets', atom=param_atom, title='targets')
+	param_node.targets.append(targets)
+
+	hdf5_file.close()
+	print '' # newline
+
+def create_stratified_partition(hdf5, partition_save_prefix, train_prop=0.5, valid_prop=0.25, test_prop=0.25, tframes=1):
+	
+	nfolds = int(np.reciprocal(test_prop))
+
+	if np.linalg.norm(np.sum((train_prop, valid_prop, test_prop))-1)>1e-6:
+		raise ValueError('train_prop + valid_prop + test_prop must add up to 1')	
+	
+	if np.linalg.norm(nfolds - np.reciprocal(test_prop))>1e-6:
+		raise ValueError('Increase precision of test_prop')
+
+	# extract metadata from dataset
+	hdf5_file = tables.open_file(hdf5, mode='r')
+	param     = hdf5_file.get_node('/', 'Param')	
+	file_dict = param.file_dict[0]
+	
+	train_list = [[] for i in xrange(nfolds)]
+	valid_list = [[] for i in xrange(nfolds)]
+	test_list  = [[] for i in xrange(nfolds)]
+
+	rng = np.random.RandomState(111)
+	for key, files in file_dict.iteritems(): # for all files that share a given label
+		nfiles = len(files)		
+		ntest  = nfiles // nfolds
+		ntrain = int(nfiles * train_prop)
+		nvalid = nfiles - ntest - ntrain
+		
+		perm = rng.permutation(nfiles)		
+		for fold in range(nfolds):
+			sup         = fold*ntest + np.arange(ntest)
+			test_index  = perm[sup]						
+			rest_index  = np.setdiff1d(perm, test_index)
+			train_index = rest_index[:ntrain]
+			valid_index = rest_index[ntrain:]
+			
+			train_list[fold].append([files[i] for i in train_index])
+			valid_list[fold].append([files[i] for i in valid_index])
+			test_list[fold].append([files[i] for i in test_index])
+
+	# flatten lists
+	for fold in xrange(nfolds):
+		train_list[fold] = sum(train_list[fold],[])
+		valid_list[fold] = sum(valid_list[fold],[])
+		test_list[fold]  = sum(test_list[fold],[])
+
+	for fold, (train, valid, test) in enumerate(zip(train_list, valid_list, test_list)):
+		partition_save_name = os.path.splitext(partition_save_prefix)[0] + '-fold-%d_of_%d.pkl' % (fold+1, nfolds)
+		
+		if os.path.exists(partition_save_name):
+			warnings.warn('partition file {} already exists, new file will not be created'.format(partition_save_name))
+			continue
+		else:		
+			create_partition(hdf5, partition_save_name, train, valid, test, tframes)
+			print 'Created stratified partition %s' % partition_save_name
+	
+	hdf5_file.close()
+
+def create_partiton_from_files(hdf5, partition_save_name, train_file, valid_file, test_file, tframes=1):
+	
+	with open(train_file) as f: 
+		train_list = [line.strip() for line in f.readlines()]
+	with open(valid_file) as f: 
+		valid_list = [line.strip() for line in f.readlines()]
+	with open(test_file) as f:  
+		test_list = [line.strip() for line in f.readlines()]
+
+	create_partition(hdf5, partition_save_name, train_list, valid_list, test_list, tframes)
+
+def create_partition(hdf5, partition_save_name, train_list, valid_list, test_list, tframes):
+	
+	if os.path.exists(partition_save_name):
+		warnings.warn('partition file %s already exists, new file will not be created' % partition_save_name)
+		return
+
+	hdf5_file = tables.open_file(hdf5, mode='r')
+	data   = hdf5_file.get_node('/', 'Data')
+	nfeats = data.X.shape[1]
+	param  = hdf5_file.get_node('/', 'Param')
+	file_index = param.file_index[0]	
+
+	train_support = []
+	for f in train_list:
+		offset, nframes, key, target = file_index[f]
+		train_support.append(offset + np.arange(0,nframes,tframes))	
+	train_support = np.hstack(train_support)
+	
+	valid_support = []
+	for f in valid_list:
+		offset, nframes, key, target = file_index[f]
+		valid_support.append(offset + np.arange(0,nframes,tframes))	
+	valid_support = np.hstack(valid_support)
+
+	test_support = []
+	for f in test_list:
+		offset, nframes, key, target = file_index[f]
+		test_support.append(offset + np.arange(0,nframes,tframes))	
+	test_support = np.hstack(test_support)
+
+	# compute mean and std for training set only
+	sum_x  = np.zeros(nfeats, dtype=np.float32)
+	sum_x2 = np.zeros(nfeats, dtype=np.float32)
+	nsamples = len(train_support)*tframes
+	
+	for n,i in enumerate(train_support):
+		sys.stdout.write('\rComputing mean and variance of training set: %2.2f%%' % (n*tframes/float(nsamples)*100))
+		sys.stdout.flush()
+		for j in xrange(tframes):						
+			
+			fft_frame = data.X[i+j,:]
+			sum_x  += fft_frame
+			sum_x2 += fft_frame**2
+	print ''
+
+	mean = sum_x / nsamples
+	var  = (sum_x2 - sum_x**2/nsamples)/(nsamples-1)
+
+	# compute PCA whitening matrix
+	XX = 0
+	for n,i in enumerate(train_support):
+		sys.stdout.write('\rComputing PCA matrix: %2.2f%%' % (n*tframes/float(nsamples)*100))
+		sys.stdout.flush()
+		for j in xrange(tframes):						
+			
+			fft_frame = data.X[i+j,:]
+			X = np.reshape(fft_frame - mean, (len(fft_frame), 1))
+			XX += X.dot(X.T)
+	print ''
+	XX /= nsamples
+
+	U,S,V = np.linalg.svd(XX)
+
+	config = {
+		'hdf5': hdf5,
+		'test':  test_support, 
+		'train': train_support, 
+		'valid': valid_support,
+		'train_files' : train_list,
+		'valid_files' : valid_list,
+		'test_files' : test_list,
+		'mean':  mean,
+		'var':   var,
+		'tframes' : tframes,
+		'U' : U,
+		'S' : S
+		}
+
+	# pickle config		
+	with open(partition_save_name, 'w') as f:
+		cPickle.dump(config, f, protocol=2)
+	
+	hdf5_file.close()
+
+if __name__=='__main__':
+	
+	import sys, argparse
+
+	parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,
+		description='''Script to prepare an audio dataset. Example usage:
+		python prepare_dataset2.py ~/Datasets/tzanetakis_genre ~/Datasets/tzanetakis_genre/label_list.txt --train_prop 0.5 --valid_prop 0.25 --test_prop 0.25
+		python prepare_dataset2.py ~/Datasets/tzanetakis_genre ~/Datasets/tzanetakis_genre/label_list.txt --train train_filtered2.txt --valid valid_filtered2.txt --test test_filtered2.txt
+		''')
+
+	parser.add_argument('datadir', help='Path to dataset')
+	parser.add_argument('labels', help="A CSV file listing dataset's categorical labels")
+	parser.add_argument('--hdf5', help='Name of hdf5 file to use')
+
+	#parser.add_argument('--user-defined-partition', action='store_true', help='Use the --train, --valid, --')
+	parser.add_argument('--train')
+	parser.add_argument('--valid')
+	parser.add_argument('--test')
+
+	#parser.add_argument('--use-stratified-cv', action='store_true', help='Automatically generate partitions using stratified cross-validation')
+	parser.add_argument('--train_prop', type=float)
+	parser.add_argument('--valid_prop', type=float)
+	parser.add_argument('--test_prop', type=float)
+
+	parser.add_argument('--partition_name')
+	parser.add_argument('--tframes', type=int)
+
+	args = parser.parse_args()
+	
+	# check validity of arugments
+	if len(sys.argv) < 3:
+		parser.error('must specify either --train,--valid,--test files or --train_prop, --valid_prop, --test_prop')
+
+	if (args.train is not None or args.valid is not None or args.test is not None) \
+	and (args.train_prop is not None or args.valid_prop is not None or args.test_prop is not None):
+		parser.error('either specify user supplied files with --train, --valid, --test, OR specify or use --train_prop, --valid_prop, --test_prop to automatically generate stratified partitions')
+
+	if (args.train is not None or args.valid is not None or args.test is not None) \
+	and (args.train is None or args.valid is None or args.test is None):
+		parser.error('if any of the flags --train, --valid, --test are specified, then they all must be specified')
+
+	if (args.train_prop is not None or args.valid_prop is not None or args.test_prop is not None) \
+	and (args.train_prop is None or args.valid_prop is None or args.test_prop is None):
+		parser.error('if any of the flags --train_prop, --valid_prop, --test_prop are specified, then they all must be specified')
+
+	# substitute defaults for missing values
+	if args.hdf5 is None:
+		dataset_name = os.path.split(os.path.abspath(args.datadir))[-1] + '.h5'
+		args.hdf5 = os.path.join(args.datadir, dataset_name)
+	
+	if args.partition_name is None:
+		dataset_name = os.path.split(os.path.abspath(args.datadir))[-1] + '.pkl'
+		args.partition_name = os.path.join(args.datadir, dataset_name)
+
+	if args.tframes is None:
+		args.tframes = 1
+
+	with open(args.labels) as f:
+		label_list = f.readline()
+		label_list = label_list.replace(' ','').split(',')
+		print 'Using labels:', label_list
+
+	# create partitions
+	print 'Preparing hdf5 file'
+	make_hdf5(args.hdf5, label_list, args.datadir)
+
+	if args.train is not None:
+		'Print creating partition %s from files %s, %s, %s' % (args.partition_name, args.train, args.valid, args.test)
+		create_partiton_from_files(args.hdf5, args.partition_name, args.train, args.valid, args.test, args.tframes)
+	
+	elif args.train_prop is not None:
+		'Print creating stratified partitions'
+		create_stratified_partition(args.hdf5, args.partition_name, args.train_prop, args.valid_prop, args.test_prop, args.tframes)
+
