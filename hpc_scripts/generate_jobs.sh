fc2e4b13599479df7fd8468db1e4b5e6c62ebba8
Bob Sturm <sturm@frank.eecs.qmul.ac.uk>
2015-02-24 09:26:00 +0000

    added type=int to nfft and nhop

--- hpc_scripts/generate_jobs.sh
+++ hpc_scripts/generate_jobs.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-python prepare_dataset.py \
-        ../GTZAN \
-        ./label_list_GTZAN.txt \
-        --hdf5 ./GTZAN.hdf5 \
-	--nfft 4096 --nhop 2048 \
-        --train ./gtzan/train_stratified2.txt \
-        --valid ./gtzan/valid_stratified2.txt \
-        --test ./gtzan/test_stratified2.txt \
-        --partition_name ./GTZANstrat_partition_configuration.pkl
+#python prepare_dataset.py \
+#        ../GTZAN \
+#        ./label_list_GTZAN.txt \
+#        --hdf5 ./GTZAN.hdf5 \
+#	--nfft 4096 --nhop 2048 \
+#        --train ./gtzan/train_stratified2.txt \
+#        --valid ./gtzan/valid_stratified2.txt \
+#        --test ./gtzan/test_stratified2.txt \
+#        --partition_name ./GTZANstrat_partition_configuration.pkl
+
+python train_mlp_script.py \
+        ./GTZANstrat_partition_configuration.pkl \
+        ./yaml_scripts/mlp_rlu2.yaml \
+        --nunits 50
+        --output GTZAN_strat2049_model.pkl
