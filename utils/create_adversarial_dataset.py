64764b0dcd773526fcb1bb8009a7318187e0c92a
coreyker <coreyker@gmail.com>
2015-06-26 15:18:57 +0200

    na

--- utils/create_adversarial_dataset.py
+++ utils/create_adversarial_dataset.py
@@ -121,11 +121,12 @@ def file_misclass_error_printf(dnn_model, root_dir, dataset, save_file, mode='al
                 fwd_xform=fwd_xform,
                 back_xform=back_xform,
                 P0=np.hstack((Phs, -Phs[:,-2:-dataset.nfft/2-1:-1])), 
-                mu=.15, 
+                mu=.01, 
                 epsilon=epsilon, 
-                maxits=10, 
-                stop_thresh=0.9, 
-                griffin_lim=True)
+                maxits=50, 
+                stop_thresh=0.5, 
+                griffin_lim=False#True
+                )
             
             if save_adversary_audio: 
                 
@@ -221,7 +222,7 @@ if __name__ == '__main__':
         save_file=args.dnn_save_file, 
         mode=args.mode, 
         label=args.label, 
-        snr=15., 
+        snr=-300.,#15., 
         aux_model=aux_model, 
         aux_save_file=args.aux_save_file, 
         which_layers=args.which_layers,
